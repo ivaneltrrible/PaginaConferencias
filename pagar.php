2933364ed430df6e8d7f513a8f1d66e8d77dec61
ivaneltrrible <lucio_21@live.com.mx>
2020-04-29 22:51:25 -0500

    PaypalArticulos

--- pagar.php
+++ pagar.php
@@ -27,20 +27,28 @@ if (isset($_POST['submit'])) :
     $total = $_POST['total_pedido'];
     $fecha = date('Y-m-d H:i:s');
     $boletos = $_POST['boletos'];
-    $camisas = $_POST['pedido_extra'];
-    $etiquetas = $_POST['pedido_extra'];
+    $numero_boletos = $boletos;  // variable para no utilizar $boletos y asi no confundir o cambiar datos
+
+    $pedido_extra = $_POST['pedido_extra'];  // variable para reutilizar etiquetas y camisas juntas 
+    $camisas = $_POST['pedido_extra']["camisas"]["cantidad"];
+    $precioCamisas = $_POST['pedido_extra']["camisas"]["precio"];
+
+    $etiquetas = $_POST['pedido_extra']["etiquetas"]["cantidad"];
+    $precioEtiquetas = $_POST['pedido_extra']["etiquetas"]["precio"];
+
     include_once 'includes/funciones/funciones.php';
     $pedidos = productos_json($boletos, $camisas, $etiquetas);
+
     if (isset($_POST['registro'])) {
         $eventos = $_POST['registro'];
     };
     $registro = eventos_json($eventos);
 
+
     echo '<pre>' ;
-     var_dump($_POST) ;
+     var_dump($pedido_extra) ;
     echo '</pre>' ;
-    exit;
-endif;
+    
 try {
     require_once 'includes/funciones/db_conexion.php';
     $stmt = $conn->prepare("INSERT INTO registrados(nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ");
@@ -48,12 +56,11 @@ try {
     $stmt->execute();
     $stmt->close();
     $conn->close();
-    header('Location: validar_registro.php?exitoso=1');
+   // header('Location: validar_registro.php?exitoso=1');
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
-
-
+endif;
 
 
 
@@ -62,19 +69,65 @@ try {
 $compra = new Payer();
 $compra->setPaymentMethod('paypal');
 
-/*
-//SE CREA UNA INSTANCIA DEL OBEJTO ITEM que practicamente contiene descripcion,nombre, etc
 $articulo = new Item();
-//NOMBRE DE PRODUCTO
 $articulo->setName($producto)
-//TIPO DE MONEDA
-->setCurrency('MXN')
-//CANTIDAD DE PRODUCTOS PUEDE LLEVAR UNA VARIABLE
-->setQuantity(1)
-//PRECIO DE PRODUCTO O ITEM
-->setPrice($precio);
+            //TIPO DE MONEDA
+            ->setCurrency('MXN')
+            //CANTIDAD DE PRODUCTOS PUEDE LLEVAR UNA VARIABLE
+            ->setQuantity(1)
+            //PRECIO DE PRODUCTO O ITEM
+            ->setPrice($precio);
 
 
+//SE CREA UN FOREACH PARA RECORRER CADA PRODUCTO QUE SELECCIONE CREARLO Y DE ESTA MANERA NO CREAR PRODUCTO POR PRODUCTO DEPENDIENDO LOS BOLETOS SELECCIONADOS
+$i = 0;
+foreach ($numero_boletos as $key => $value) {
+    if ( (int) $value["cantidad"] > 0) {
+
+        //SE CREA UNA INSTANCIA DEL OBEJTO ITEM que practicamente contiene descripcion,nombre, etc
+        ${"articulo".$i} = new Item();
+        //NOMBRE DE PRODUCTO
+        ${"articulo".$i}->setName('Pase ' . $key)
+            //TIPO DE MONEDA
+            ->setCurrency('MXN')
+            //CANTIDAD DE PRODUCTOS PUEDE LLEVAR UNA VARIABLE
+            ->setQuantity( (int) $value['cantidad'])
+            //PRECIO DE PRODUCTO O ITEM
+            ->setPrice( (int) $value['precio']);
+        $i++;
+       
+    }
+}
+
+//RECORRER TODAS LAS ETIQUETAS O CAMISAS SELECCIONADAS PARA CREAR ITEM DE CADA UNA SI ES QUE HAY
+foreach ($pedido_extra as $key => $value) {
+    if ( (int) $value["cantidad"] > 0) {   
+ 
+       if ($key == 'camisas') {
+           $precio = (float) $value['precio'] * .93;
+           
+       } else{
+           $precio = (int) $value['precio'];
+       }
+        //SE CREA UNA INSTANCIA DEL OBEJTO ITEM que practicamente contiene descripcion,nombre, etc
+        ${"articulo".$i} = new Item();
+        //NOMBRE DE PRODUCTO
+        ${"articulo".$i}->setName('Extra: ' . $key)
+            //TIPO DE MONEDA
+            ->setCurrency('MXN')
+            //CANTIDAD DE PRODUCTOS PUEDE LLEVAR UNA VARIABLE
+            ->setQuantity( (int) $value['cantidad'])
+            //PRECIO DE PRODUCTO O ITEM
+            ->setPrice($precio);
+           
+        $i++;
+      
+       
+    }
+}
+
+
+/*
 //Segundo Articulo ejemplo
 $articulo_ropa = new Item();
 $articulo_ropa->setName('jeans')->setCurrency('MXN')->setPrice($precio_ropa)->setQuantity(1);
